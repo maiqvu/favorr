31c68e517b0b306e96b765de81fba4f49f136627
Alex Au <alexaumh@gmail.com>
2020-08-24 00:07:58 +1000

    Add paths to public request page and create public request page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CreatePublicRequestPage from './pages/CreatePublicRequestPage';
+import PublicRequestPage from './pages/PublicRequestPage';
+import NavBar from './NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div>
+            <Route path="/" component={PublicRequestPage} exact />
+            <Route path="/createPublicRequest" component={CreatePublicRequestPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
