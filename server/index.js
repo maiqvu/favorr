7f4c630894518739ecc396655c0b0d048ac62d9c
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-08-24 23:45:41 +1000

    Add Passport for JWT authentication

--- server/index.js
+++ server/index.js
@@ -1,33 +1,41 @@
 // Import dependencies
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-require('dotenv').config();
+import passport from 'passport';
+
 
 // Import route handlers
-import authRouter from './routes/auth.route';
-import userRouter from './routes/users.route';
+import users from './routes/users.route';
 import requestRouter from './routes/publicRequest.route';
+
 
 // Initialize an Express app
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
+app.use(passport.initialize());
+require('./utils/passport')(passport);
+
 
 // Connect to database
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to database.'))
   .catch(err => console.log(`Failed to connect to database: ${err}`));
 
-// Routing
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/publicRequest', requestRouter);
 
+// Routing
+app.use('/api/users', users);
+app.use('/api/publicRequest', requestRouter);
+
+
+// Start the Express server
 app.listen(process.env.PORT, err => {
   if (err) {
     console.log(err);
