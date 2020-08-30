d6fe453d5dd38d292ccb7a92c965971e60ed3fd2
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-08-30 23:31:10 +1000

    Refactor Passport config for cleaner code & better error handling

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -4,17 +4,23 @@ import User from '../models/user.model';
 
 
 export const applyPassportStrategy = passport => {
-  const options = {};
-  options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  options.secretOrKey = process.env.JWT_SECRET;
+  passport.use(new Strategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET
+    },
+    async (payload, done) => {
+      try {
+        const user = await User.findOne({ email: payload.email })
 
-  passport.use(
-    new Strategy(options, (payload, done) => {
-      User.findOne({ email: payload.email }, (err, user) => {
-        if (err) return done(err, false);
-        if (user) return done(null, { _id: user._id, email: user.email });
-        return done(null, false);
-      });
-    })
-  );
+        // If user doesn't exist, handle it
+        if (!user) return done(null, false);
+
+        // Otherwise, return the user object
+        done(null, { _id: user._id, email: user.email });
+      } catch (err) {
+        done(err, false);
+      }
+    }
+  ));
 };
