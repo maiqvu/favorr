98307e268e947807520a233325573aa9444add2a
Alex Au <alexaumh@gmail.com>
2020-09-03 00:13:25 +1000

    Improve public request page layout

--- client/src/components/Request/PublicRequestPage.js
+++ client/src/components/Request/PublicRequestPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -30,22 +29,45 @@ export default class PublicRequestPage extends Component {
 
   async addReward(requestId) {
     try {
-      await axios.post(`/api/publicRequest/${requestId}/add-reward`, {
-        name: 'Alex',
-        item: this.state.newReward,
-      });
-      this.componentDidMount();
+      let response = await axios.post(
+        `/api/publicRequest/${requestId}/add-reward`,
+        {
+          name: 'Alex',
+          item: this.state.newReward,
+        }
+      );
+      for (let request of response.data) {
+        request['expanded'] = false;
+      }
+      this.setState({ requestList: response.data });
     } catch (err) {
       console.error(err);
     }
   }
 
-  constructRewardItemList(rewards) {
+  constructFullRewardItemList(rewards) {
     let rewardItemList = [];
     rewards.map((reward) => {
       rewardItemList.push(reward.item);
     });
-    return rewardItemList.toString();
+    return rewardItemList.toString().replace(/,/g, ', ');
+  }
+
+  constructLimitedRewardItemList(rewards) {
+    let displayList;
+    let rewardItemList = [];
+    rewards.map((reward) => {
+      rewardItemList.push(reward.item);
+    });
+
+    if (rewardItemList.length > 2) {
+      displayList =
+        rewardItemList.slice(0, 2).toString().replace(/,/g, ', ') + '...';
+    } else {
+      displayList = rewardItemList.toString();
+    }
+
+    return displayList;
   }
 
   expandRequestToggle(requestId) {
@@ -54,59 +76,83 @@ export default class PublicRequestPage extends Component {
     newRequestList.forEach((part, index) => {
       if (part._id === requestId) {
         part.expanded = !part.expanded;
+      } else {
+        part.expanded = false;
       }
     }, newRequestList);
 
+    this.setState({ newReward: '' });
     this.setState({ requestList: newRequestList });
-  }
-
-  rowStyleFormat(row, rowIdx) {
-    return { backgroundColor: rowIdx % 2 === 0 ? 'red' : 'blue' };
   }
 
   render() {
     return (
       <div>
         <h3>All Requests</h3>
-        <Container>
+        <Container className="px-lg-5">
           <Row>
-            <Col className="col-sm-1">Creator</Col>
-            <Col className="col-sm-1">Date Submitted</Col>
-            <Col className="col-sm-4">Task</Col>
-            <Col className="col-sm-3">Rewards</Col>
-            <Col className="col-sm-3"></Col>
+            <Col className="col-sm-5 text-left font-weight-bold">Task</Col>
+            <Col className="col-sm-3 text-left font-weight-bold">Date Submitted</Col>
+            <Col className="col-sm-3 text-left font-weight-bold">Rewards</Col>
+            <Col className="col-sm-1 text-right"></Col>
           </Row>
+          <hr />
           {this.state.requestList.map((request) => (
             <React.Fragment>
-              <Row Style={this.rowStyleFormat}>
-                <Col className="col-sm-1">{request.creator}</Col>
-                <Col className="col-sm-1">30/08/2020</Col>
-                <Col className="col-sm-4">{request.requestDetail}</Col>
-                <Col className="col-sm-3">
-                  {this.constructRewardItemList(request.reward)}
+              <Row>
+                <Col className="col-sm-5 text-left">
+                  {request.requestDetail}
                 </Col>
-                <Col className="col-sm-3">
-                  <div className="text-right">
-                    <Button variant="outline-success">Claim</Button>
-                    <Button
-                      variant="outline-primary"
-                      onClick={() => this.expandRequestToggle(request._id)}
-                    >
-                      Add Reward
-                    </Button>
-                  </div>
+                <Col className="col-sm-3 text-left">30/08/2020</Col>
+                <Col className="col-sm-3 text-left">
+                  {this.constructLimitedRewardItemList(request.reward)}
+                </Col>
+                <Col className="col-sm-1 text-right">
+                  <button
+                    onClick={() => this.expandRequestToggle(request._id)}
+                    type="button"
+                    class="btn dropdown-toggle"
+                    data-toggle="dropdown"
+                  >
+                    <span class="caret"></span>
+                  </button>
                 </Col>
               </Row>
               {request.expanded ? (
-                <Row>
-                  <Col className="col-sm-1"></Col>
-                  <Col className="col-sm-1"></Col>
-                  <Col className="col-sm-4"></Col>
-                  <Col className="col-sm-3"></Col>
-                  <Col className="col-sm-3">
+                <div className="p-3 mb-2 bg-light text-dark">
+                  <h5 className="p-2 col-example text-left">
+                    REQUEST DETAILS:
+                  </h5>
+                  <div className="d-flex justify-content-between">
+                    <div>
+                      <div className="p-2 col-example text-left">
+                        <strong>Creator:&ensp;</strong>
+                        <span>{request.creator}</span>
+                      </div>
+                      <div className="p-2 col-example text-left">
+                        <strong>Task:&ensp;</strong>
+                        <span>{request.requestDetail}</span>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="p-2 col-example text-left">
+                        <strong>Date Submitted:&ensp;</strong>
+                        <span>30/08/2020</span>
+                      </div>
+                      <div className="p-2 col-example text-left">
+                        <strong>Rewards:&ensp;</strong>
+                        <span>
+                          {this.constructFullRewardItemList(request.reward)}
+                        </span>
+                      </div>
+                    </div>
+                    <div></div>
+                  </div>
+                  <div className="p-2 col-example text-left">
+                    <strong>Add Reward:&ensp;</strong>
                     <form
                       class="form-inline"
-                      className="text-right"
+                      className="text-left"
                       onSubmit={() => this.addReward(request._id)}
                     >
                       <input
@@ -118,13 +164,15 @@ export default class PublicRequestPage extends Component {
                           this.setState({ newReward: event.target.value });
                         }}
                       />
-                      <Button type="submit" class="btn">
+                      &ensp;
+                      <button class="btn btn-outline-primary" type="submit">
                         Add
-                      </Button>
+                      </button>
                     </form>
-                  </Col>
-                </Row>
+                  </div>
+                </div>
               ) : null}
+              <hr />
             </React.Fragment>
           ))}
         </Container>
