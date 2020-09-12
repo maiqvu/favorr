f49d0d1deae582050317d50a6574c5c287008ce2
Alex <alexaumh@gmail.com>
2020-09-12 23:44:10 +1000

    Update import paths and links to point to new folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import CreatePublicRequestPage from './components/Request/CreatePublicRequestPage';
-import PublicRequestPage from './components/Request/PublicRequestPage';
-import Login from './components/Request/Login';
-import Register from './components/Request/Register';
+import AvailablePublicRequests from './components/PublicRequest/AvailablePublicRequests';
+import CreatePublicRequest from './components/PublicRequest/CreatePublicRequest';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import NavBar from './NavBar';
 import './App.css';
 
@@ -18,8 +18,8 @@ class App extends Component {
           <NavBar />
           <Layout
           ><Switch>
-              <Route exact path="/" component={PublicRequestPage} />
-              <Route path="/createPublicRequest" component={CreatePublicRequestPage} />
+              <Route exact path="/" component={AvailablePublicRequests} />
+              <Route path="/createPublicRequest" component={CreatePublicRequest} />
               <Route path="/login" component={Login} />
               <Route path="/register" component={Register} />
             </Switch>
