1c5db88b40ba5363908448ca3cfc8549286eaee8
Alex Au <alexaumh@gmail.com>
2020-09-14 21:29:51 +1000

    Update navbar to toggle when screen width is xs

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 function NavBar() {    
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">Favorr</Link>
-            <div class="collapse navbar-collapse">
+            <Button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </Button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <Link className="nav-item nav-link" to="/">Public Request</Link>
                     <Link className="nav-item nav-link" to="/createPublicRequest">Create Public Request</Link>
