e1019b5e40ff000b28744614d4cb0e2d7cec1296
john-tng1 <13621242@student.uts.edu.au>
2020-09-15 00:54:49 +1000

    Refactor CreatePublicRequest with react-bootstrap
    need help debugging, error 500 when submitting form

--- client/src/components/PublicRequest/CreatePublicRequest.js
+++ client/src/components/PublicRequest/CreatePublicRequest.js
@@ -1,44 +1,41 @@
 import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Select from 'react-select';
+import { Form, Button, Modal } from 'react-bootstrap';
+
 import axios from 'axios';
-
-const rewardOptions = [
-  { value: 'Chocolate', label: 'Chocolate' },
-  { value: 'Strawberry', label: 'Strawberry' },
-  { value: 'Coke', label: 'Coke' },
-  { value: 'Breakfast', label: 'Breakfast' },
-  { value: 'Coffee', label: 'Coffee' },
-  { value: 'Candy', label: 'Candy' }
-]
-
 export default class CreatePublicRequestPage extends Component {
   constructor(props) {
-    super();
+    super(props);
 
-    this.user = 'alex';
     this.state = {
-      task: '',
+      creator: 'John',
+      requestDetail: '',
       reward: '',
+      taker: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange = event => {
-    
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
-  handleSubmit = event => {
+
+  handleSubmit = async event => {
+    const { creator, taker, requestDetail, reward } = this.state;
     event.preventDefault();
-    const data = {
-      name: this.user,
-      requestDetail: this.state.task,
-      reward: this.state.reward,
-    }
-    axios
-      .post('/api/publicRequest/publicRequest', data)
-      .then(res => console.log(res));
+
+    await axios
+      .post(`/api/publicRequest/publicRequest`,
+        {
+          creator: creator,
+          requestDetail: requestDetail,
+          reward: reward,
+          taker: taker
+        })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   render() {
@@ -47,23 +44,29 @@ export default class CreatePublicRequestPage extends Component {
         <h3>Create a Public Request</h3>
         <Form.Group controlId="taskDetail">
           <Form.Label>Task Details:</Form.Label>
-          <Form.Control
-            required
-            as="textarea"
+          <Form.Control as="textarea"
+            //required
+            name="requestDetail"
             rows="2"
-            value={this.state.task}
+            value={this.state.requestDetail}
             onChange={this.handleChange}
           />
         </Form.Group>
-        <Form.Group controlId="dropdownReward">
-          <Form.Label>Reward(s):</Form.Label>
-          <Select
-            isSearchable
-            isMulti
-            options={rewardOptions}
-            value={this.props.value}
+        <Form.Group controlId="selectReward">
+          <Form.Label>Reward:</Form.Label>
+          <Form.Control as="select"
+            name="reward"
+            value={this.state.reward}
             onChange={this.handleChange}
-          />
+            custom
+          >
+            <option value="">Add a reward</option>
+            <option value="Chocolate Bar">Chocolate Bar</option>
+            <option value="Orange Juice">Orange Juice</option>
+            <option value="Chips">Chips</option>
+            <option value="Candy">Candy</option>
+            <option value="Breakfast">Breakfast</option>
+          </Form.Control>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
