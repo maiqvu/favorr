ced6e594ec7a54d0f6689d695d47f58378d8b7fa
Alex Au <alexaumh@gmail.com>
2020-09-17 00:41:12 +1000

    Add MyClaimedRequests page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AvailablePublicRequests from './components/PublicRequest/AvailablePublicRequests';
 import CreatePublicRequest from './components/PublicRequest/CreatePublicRequest';
+import MyClaimedRequests from './components/PublicRequest/MyClaimedRequests';
 import MyFavors from './components/Favor/MyFavors';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
@@ -20,6 +21,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={AvailablePublicRequests} />
               <Route path="/createPublicRequest" component={CreatePublicRequest} />
+              <Route path="/myClaimedRequests" component={MyClaimedRequests} />
               <Route path="/myFavors" component={MyFavors} />
               <Route path="/login" component={Login} />
               <Route path="/register" component={Register} />
