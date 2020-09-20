00a16491d0a4434900be3fc8d77262949cd949a1
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-09-20 14:49:20 +1000

    Refactor MyClaimedRequests to use global auth context

--- client/src/components/PublicRequest/MyClaimedRequests.js
+++ client/src/components/PublicRequest/MyClaimedRequests.js
@@ -1,74 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-
 import Request from './Request';
+import { AuthContext } from '../../context/AuthContext';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default class MyClaimedRequests extends Component { 
-    constructor(props) {
-        super(props);
-
-        this.username = 'john';
-        this.state = {
-            claimedRequests: [],
-            focusedRequestId: ''
-        }
+const MyClaimedRequests = (props) => {
+  const [ claimedRequests, setClaimedRequests ] = useState([]);
+  const [ focusedRequestId, setFocusedRequestId ] = useState('');
+  
+  const authContext = useContext(AuthContext);
+  
+  useEffect(() => {
+    axios.get(`/api/publicRequests/claimed/john`)
+    // axios.get(`/api/publicRequests/claimed/${authContext.user._id}`)
+      .then(res => setClaimedRequests(res.data))
+      .catch(err => console.error(err));
+  }, [authContext.user._id]);
+  
+  // This function is a duplicate from AvailablePublicRequest
+  const expandRequestToggle = (requestId) => {
+    if (focusedRequestId === requestId) {
+      setFocusedRequestId(null);
+    } else {
+      setFocusedRequestId(requestId);
     }
+  };
+  
+  return (
+    <div>
+      <h3 className="text-center">My Claimed Requests</h3>
+      
+      <Container className="px-lg-5">
+        <Row>
+          <Col className="col-sm-5 text-left font-weight-bold">
+            Task
+          </Col>
+          <Col className="col-sm-3 text-left font-weight-bold">
+            Date Claimed
+          </Col>
+          <Col className="col-sm-3 text-left font-weight-bold">
+            Rewards
+          </Col>
+        </Row>
+        <hr className="border border-light" />
+        
+        {claimedRequests.map((claimedRequest, index) =>
+          <Request
+            key={index}
+            request={claimedRequest}
+            date={claimedRequest.claimedByTime}
+            username={authContext.user.username}
+            focusedRequestId={focusedRequestId}
+            // updateRequest={updateRequest}
+            expandRequestToggle={() => expandRequestToggle(claimedRequest._id, index)} />
+        )}
+      </Container>
+    </div>
+  );
+};
 
-    async componentDidMount() {
-        try {
-            let response = await axios.get(`/api/publicRequests/claimed/${this.username}`);
-            this.setState({ claimedRequests: response.data });
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    // This function is a duplicate from AvailablePublicRequest
-    expandRequestToggle(requestId) {
-        if (this.state.focusedRequestId === requestId) {
-          this.setState({ focusedRequestId: null });
-        } else {
-          this.setState({ focusedRequestId: requestId });
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h3 className="text-center">My Claimed Requests</h3>
-                <Container className="px-lg-5">
-                    <Row>
-                        <Col className="col-sm-5 text-left font-weight-bold">
-                            Task
-                        </Col>
-                        <Col className="col-sm-3 text-left font-weight-bold">
-                            Date Claimed
-                        </Col>
-                        <Col className="col-sm-3 text-left font-weight-bold">
-                            Rewards
-                        </Col>
-                        <Col className="col-sm-1 text-right"></Col>
-                    </Row>
-                    <hr className="border border-light" />
-                    {this.state.claimedRequests.map((claimedRequest, index) =>
-                        <Request
-                            request={claimedRequest}
-                            date={claimedRequest.claimedByTime}
-                            index={index}
-                            username={this.username}
-                            focusedRequestId={this.state.focusedRequestId}
-                            updateRequest={this.updateRequest}
-                            expandRequestToggle={() =>
-                                this.expandRequestToggle(claimedRequest._id, index)
-                            }
-                        />
-                    )}
-                </Container>
-            </div>
-        )
-    }
-}
+export default MyClaimedRequests;
