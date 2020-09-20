eb796958f1970883206d8dc6677894110ccd80bb
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-09-20 10:06:26 +1000

    Add passport-local for authorization

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -1,26 +1,46 @@
 import 'dotenv/config';
-import { Strategy, ExtractJwt } from 'passport-jwt';
+import passport from 'passport';
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import { Strategy as LocalStrategy } from 'passport-local';
 import User from '../models/user.model';
 
-
-export const applyPassportStrategy = passport => {
-  passport.use(new Strategy(
-    {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET
-    },
-    async (payload, done) => {
-      try {
-        const user = await User.findOne({ email: payload.email })
-
-        // If user doesn't exist, handle it
-        if (!user) return done(null, false);
-
-        // Otherwise, return the user object
-        done(null, { _id: user._id, email: user.email });
-      } catch (err) {
-        done(err, false);
-      }
-    }
-  ));
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['access_token'];
+  }
+  return token;
 };
+
+
+// Middleware for authentication strategy
+passport.use(new JwtStrategy(
+  {
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: process.env.JWT_SECRET
+  },
+  (payload, done) => {
+    User.findById({_id: payload.sub}, (err, user) => {
+      if (err) return done(err, false);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  }
+));
+
+// Middleware for authorization
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({username}, (err, user) => {
+    // Something wrong with the database
+    if (err) return done(err);
+    
+    // If no user exists
+    if (!user) return done(null, false);
+    
+    // If user exists, verify if password is correct
+    user.comparePassword(password, done);
+  });
+}));
