1d673b3805e48d43ba69c6070e222dd340d86cc3
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-09-26 12:45:01 +1000

    Finish the logic to dynamically handle owedBy/owedTo choices and create new favor

--- client/src/components/Favor/AddFavor.js
+++ client/src/components/Favor/AddFavor.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import FavorService from './FavorService';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 
 const AddFavor = () => {
@@ -8,6 +10,22 @@ const AddFavor = () => {
     owedTo: ''
   });
   const [ owedByMe, setOwedByMe ] = useState(null);
+  const [ myFriend, setMyFriend ] = useState('');
+  
+  const authContext = useContext(AuthContext);
+  
+  useEffect(function updateMyFriend() {
+    if (owedByMe) setNewFavor({
+      ...newFavor,
+      owedBy: authContext.user.username,
+      owedTo: myFriend
+    })
+    else setNewFavor({
+      ...newFavor,
+      owedBy: myFriend,
+      owedTo: authContext.user.username
+    });
+  }, [owedByMe, myFriend]);
   
   const handleChange = (e) => {
     setNewFavor({ ...newFavor, [e.target.name]: e.target.value });
@@ -15,7 +33,7 @@ const AddFavor = () => {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(newFavor);
+    FavorService.addFavor(newFavor);
   };
   
   return (
@@ -93,6 +111,37 @@ const AddFavor = () => {
           </Form.Label>
         </div>
       </Form.Group>
+      
+      <Form.Group as={Row} controlId="setMyFriend">
+        <Form.Label column sm={2}>My friend: </Form.Label>
+        <Col sm={8}>
+          <Form.Control 
+            required
+            placeholder="Enter your friend's username"
+            name="myFriend"
+            value={myFriend}
+            onChange={e => setMyFriend(e.target.value)}
+          />
+        </Col>
+      </Form.Group>
+      
+      {/* <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1">My friend: </InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control
+          placeholder="Enter your friend's username"
+          aria-describedby="basic-addon1"
+          value={myFriend}
+          onChange={(e) => {
+            setMyFriend(e.target.value);
+            // console.log(myFriend);
+          }}
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Enter</Button>
+        </InputGroup.Append>
+      </InputGroup> */}
       
       {
         !owedByMe && 
