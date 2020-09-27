b1136f38c103cf807a1767234ab24a74ab2df0e6
Alex Au <alexaumh@gmail.com>
2020-09-27 22:45:15 +1000

    Show page not found error if url does not exist

---
+++ client/src/components/Auth/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className="grid-content small-12" style={{marginTop:"20px"}}>
+            <h3>404 page not found</h3>
+        </div>
+    )
+}
+
+export default NotFoundPage;
