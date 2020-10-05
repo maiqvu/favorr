9837c14f5cd294eab9bcfde3e9373b4a975699ea
Mai Vu <58199372+maiqvu@users.noreply.github.com>
2020-10-05 13:59:16 +1100

    Add more error handling to AddFavor

--- client/src/components/Favor/AddFavor.js
+++ client/src/components/Favor/AddFavor.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import FavorService from './FavorService';
-import { Form, Row, Col, Button, InputGroup } from 'react-bootstrap';
+import { Form, Row, Col, Button, Modal } from 'react-bootstrap';
 
 const AddFavor = () => {
   const [ newFavor, setNewFavor ] = useState({
@@ -12,49 +12,69 @@ const AddFavor = () => {
   });
   const [ owedByMe, setOwedByMe ] = useState(null);
   const [ myFriend, setMyFriend ] = useState('');
+  const [ showModal, setShowModal ] = useState(false);
+  const [ err, setErr ] = useState('');
+  
   const authContext = useContext(AuthContext);
   const history = useHistory();
   
-  const handleChange = (e) => {
+  const handleSelectFavor = (e) => {
     setNewFavor({ ...newFavor, [e.target.name]: e.target.value });
   };
   
-  const updateMyFriend = () => {
-    if (owedByMe) {
-      setNewFavor({
-        ...newFavor,
-        owedBy: authContext.user.username,
-        owedTo: myFriend
-      });
-    } else {
-      setNewFavor({
-        ...newFavor,
-        owedBy: myFriend,
-        owedTo: authContext.user.username
-      });
-    }
-  };
+  const handleSearchMyFriend = () => {
+    FavorService.findUserByUsername(myFriend).then(res => {
+      if (res.username !== authContext.user.username) {
+        setErr(null);
+        setShowModal(true);
+        
+        if (owedByMe) {
+          setNewFavor({
+            ...newFavor,
+            owedBy: authContext.user.username,
+            owedTo: myFriend
+          });
+        } else {
+          setNewFavor({
+            ...newFavor,
+            owedBy: myFriend,
+            owedTo: authContext.user.username
+          });
+        }
+      } else {
+        setErr(`Invalid search. Please enter your friend's username.`);
+        setShowModal(true);
+      }
+    }).catch(err => {
+      console.error(err.message);
+      setErr('Cannot find this username.');
+      setShowModal(true);
+    });
+  }
   
   const handleSubmit = async (e) => {
     e.preventDefault();
     await FavorService.addFavor(newFavor);
-    history.push(`/myFavors`);
+    setTimeout(() => {
+      console.log('2 seconds after favor saved in backend');
+      history.push(`/myFavors`);
+    }, 2000);
   };
   
   return (
     <Form onSubmit={handleSubmit}>
-      <h3 className="text-left font-weight-bold">Create a new favor</h3>
+      <h3 className="text-center font-weight-bold">Create a new favor</h3>
       <br/>
       
       <Form.Group as={Row}>
-        <Form.Label column sm={2}>Description</Form.Label>
+        <Form.Label column sm={2}>Description *</Form.Label>
         <Col sm={8}>
           <Form.Control 
             required
             as="select"
             name="description"
             value={newFavor.description}
-            onChange={handleChange}
+            onChange={handleSelectFavor}
           >
             <option value="">Select a favor</option>
             <option value="Brownie">Brownie</option>
@@ -67,7 +87,7 @@ const AddFavor = () => {
       </Form.Group>
 
       <Form.Group as={Row}>
-        <Form.Label as="legend" column sm="2">Owed by</Form.Label>
+        <Form.Label as="legend" column sm="2">Owed by *</Form.Label>
         <Col>
           <Form.Check
             type="radio"
@@ -87,7 +107,7 @@ const AddFavor = () => {
       </Form.Group>
       
       <Form.Group as={Row}>
-        <Form.Label as="legend" column sm="2">Owed to</Form.Label>
+        <Form.Label as="legend" column sm="2">Owed to *</Form.Label>
         <Col>
           <Form.Check
             type="radio"
@@ -106,24 +126,47 @@ const AddFavor = () => {
         </Col>
       </Form.Group>
       
-      <InputGroup as={Row}>
-        <Form.Label column sm={2}>My friend: </Form.Label>
-        <Col sm={8}>
-          <Form.Control
-            required
-            placeholder="Enter your friend's username"
-            name="myFriend"
+      <Col sm={10}>
+        <div className="input-group mb-3">
+          <input type="text"
+            className="form-control"
+            placeholder="Search your friend's username"
             value={myFriend}
             onChange={e => setMyFriend(e.target.value)}
           />
-        </Col>
-        <InputGroup.Append>
-          <Button onClick={updateMyFriend} variant="outline-primary">
-            Enter
+          <div className="input-group-append">
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={handleSearchMyFriend}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </Col>
+      
+      <Modal
+        centered
+        backdrop="static"
+        dialogClassName="modal-60w"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+      >
+        <Modal.Body>
+          {!err ? <h4>Found your friend <strong>{myFriend}</strong>!</h4> : <h4>{err}</h4>}
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            size="sm"
+            variant="outline-primary"
+            onClick={() => setShowModal(false)}
+          >
+            OK
           </Button>
-        </InputGroup.Append>
-      </InputGroup>
-      <br/>
+        </Modal.Footer>
+      </Modal>
       
       {
         !owedByMe && 
