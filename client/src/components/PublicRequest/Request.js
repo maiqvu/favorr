bfd7c9d2fa2e4f43423c2523af97a57aa8af5ed5
Alex Au <alexaumh@gmail.com>
2020-10-19 00:04:15 +1100

    Fix request toggle not expanding when changing page

--- client/src/components/PublicRequest/Request.js
+++ client/src/components/PublicRequest/Request.js
@@ -12,14 +12,13 @@ const Request = (props) => {
   const [removeRewardId, setRemoveRewardId] = useState('');
   const [showUploadOption, setShowUploadOption] = useState(false);
   const [open, setOpen] = useState(false);
-  const [focused, setFocused] = useState(false);
   const [image, setImage] = useState('');
   const [resolved, setResolved] = useState(props.request.resolved);
   const history = useHistory();
 
   useEffect(() => {
     // reset expand toggle when the request has changed
-    setFocused(false);
+    setOpen(false);
   }, [props.request._id])
 
   const handleSelectNewReward = (e) => {
@@ -32,7 +31,6 @@ const Request = (props) => {
 
   const expandRequestToggle = () => {
     setOpen(!open);
-    setFocused(!focused);
   };
 
   const toggleUploadOption = () => {
@@ -125,117 +123,115 @@ const Request = (props) => {
           </button>
         </Col>
       </Row>
-      {focused ? (
-        <Collapse in={open} className="p-3 mb-2 ">
-          <div className="bg-light text-dark" id={props.request._id}>
-            <div className="p-2">
-              <h5 className="text-left">REQUEST DETAILS:</h5>
-              <Row className="mb-4">
-                <Col className="col-sm-5">
-                  <strong>Creator:&ensp;</strong>
-                  <span>{props.request.creator.username}</span>
-                </Col>
-                <Col className="col-sm-7">
-                  <strong>Task:&ensp;</strong>
-                  <span>{props.request.task}</span>
-                </Col>
-              </Row>
-              <Row className="mb-4">
-                <Col className="col-sm-5">
-                  <strong>Time Submitted:&ensp;</strong>
-                  <span>
-                    {new Date(Date.parse(props.request.createdAt)).toString()}
-                  </span>
-                </Col>
-                <Col className="col-sm-7">
-                  <strong>Rewards:&ensp;</strong>
-                  <span>
-                    {constructFullRewardItemList(props.request.rewards)}
-                  </span>
-                </Col>
-              </Row>
-            </div>
-            {props.user ? (
-              <div>
-                {props.user.username && !props.request.claimedBy ? (
-                  <Row>
-                    <Col className="col-sm-5">
-                      <AddReward
-                        newReward={newReward}
-                        onChange={handleSelectNewReward}
-                        addReward={() =>
-                          addReward(props.request._id, props.index)
-                        }
-                      />
-                    </Col>
-                    <Col className="col-sm-5">
-                      <RemoveReward
-                        request={props.request}
-                        user={props.user}
-                        removeRewardId={removeRewardId}
-                        onChange={handleSelectRemoveRewardId}
-                        removeReward={() =>
-                          removeReward(props.request._id, props.index)
-                        }
-                      />
-                    </Col>
-                  </Row>
+      <Collapse in={open} className="p-3 mb-2 ">
+        <div className="bg-light text-dark" id={props.request._id}>
+          <div className="p-2">
+            <h5 className="text-left">REQUEST DETAILS:</h5>
+            <Row className="mb-4">
+              <Col className="col-sm-5">
+                <strong>Creator:&ensp;</strong>
+                <span>{props.request.creator.username}</span>
+              </Col>
+              <Col className="col-sm-7">
+                <strong>Task:&ensp;</strong>
+                <span>{props.request.task}</span>
+              </Col>
+            </Row>
+            <Row className="mb-4">
+              <Col className="col-sm-5">
+                <strong>Time Submitted:&ensp;</strong>
+                <span>
+                  {new Date(Date.parse(props.request.createdAt)).toString()}
+                </span>
+              </Col>
+              <Col className="col-sm-7">
+                <strong>Rewards:&ensp;</strong>
+                <span>
+                  {constructFullRewardItemList(props.request.rewards)}
+                </span>
+              </Col>
+            </Row>
+          </div>
+          {props.user ? (
+            <div>
+              {props.user.username && !props.request.claimedBy ? (
+                <Row>
+                  <Col className="col-sm-5">
+                    <AddReward
+                      newReward={newReward}
+                      onChange={handleSelectNewReward}
+                      addReward={() =>
+                        addReward(props.request._id, props.index)
+                      }
+                    />
+                  </Col>
+                  <Col className="col-sm-5">
+                    <RemoveReward
+                      request={props.request}
+                      user={props.user}
+                      removeRewardId={removeRewardId}
+                      onChange={handleSelectRemoveRewardId}
+                      removeReward={() =>
+                        removeReward(props.request._id, props.index)
+                      }
+                    />
+                  </Col>
+                </Row>
+              ) : null}
+              <div className="text-right">
+                {props.user.username &&
+                props.user.username !== props.request.creator.username &&
+                props.user._id !== props.request.claimedBy ? (
+                  <Button
+                    variant="primary"
+                    onClick={() => props.claim(props.request._id, props.index)}
+                  >
+                    Claim
+                  </Button>
                 ) : null}
-                <div className="text-right">
-                  {props.user.username &&
-                  props.user.username !== props.request.creator.username &&
-                  props.user._id !== props.request.claimedBy ? (
+                {props.user.username && !resolved &&
+                props.user._id === props.request.claimedBy ? (
+                  <div>
+                    <Button onClick={toggleUploadOption}>Resolve</Button>
+                  </div>
+                ) : null}
+                {showUploadOption ? (
+                  <div>
+                    <div className='text-left'>
+                      <label htmlFor="image">Please upload a photo proof.</label><br/>
+                    </div>
+                    <Row>
+                      <Col className="md-8 text-left">
+                        <input
+                          type="file"
+                          id="image"
+                          accept=".jpg,.png"
+                          onChange={e => setImage(e.target.files[0])}
+                        />
+                      </Col>
+                      <Col className="md-4 text-right">
+                        <Button variant="primary" disabled={!image} onClick={() => resolve(props.request._id, props.index)}>Upload</Button>
+                      </Col>
+                    </Row>
+                  </div>
+                ) : null}
+                {resolved ? (
+                  <div className='bg-light'>
+                    Go to updated favor list.{' '}
                     <Button
                       variant="primary"
-                      onClick={() => props.claim(props.request._id, props.index)}
+                      size="sm"
+                      onClick={() => history.push(`/myFavors`)}
                     >
-                      Claim
-                    </Button>
-                  ) : null}
-                  {props.user.username && !resolved &&
-                  props.user._id === props.request.claimedBy ? (
-                    <div>
-                      <Button onClick={toggleUploadOption}>Resolve</Button>
-                    </div>
-                  ) : null}
-                  {showUploadOption ? (
-                    <div>
-                      <div className='text-left'>
-                        <label htmlFor="image">Please upload a photo proof.</label><br/>
-                      </div>
-                      <Row>
-                        <Col className="md-8 text-left">
-                          <input
-                            type="file"
-                            id="image"
-                            accept=".jpg,.png"
-                            onChange={e => setImage(e.target.files[0])}
-                          />
-                        </Col>
-                        <Col className="md-4 text-right">
-                          <Button variant="primary" disabled={!image} onClick={() => resolve(props.request._id, props.index)}>Upload</Button>
-                        </Col>
-                      </Row>
-                    </div>
-                  ) : null}
-                  {resolved ? (
-                    <div className='bg-light'>
-                      Go to updated favor list.{' '}
-                      <Button
-                        variant="primary"
-                        size="sm"
-                        onClick={() => history.push(`/myFavors`)}
-                      >
-                        My Favors
-                      </Button> | Resolved
-                    </div>
-                  ) : null}
-                </div>
+                      My Favors
+                    </Button> | Resolved
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
-       </Collapse>
-      ) : null} 
+            </div>
+          ) : null}
+        </div>
+      </Collapse>
       <hr />
     </React.Fragment>
   );
